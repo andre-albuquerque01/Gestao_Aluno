69e45fe5e0685fb6c7980a5677c26c7b6cb5ed1d
andre-albuquerque01 <andrealbuquerque2001@gmail.com>
2023-09-16 17:32:52 -0300

    <FEAT> Iniciando o projeto

---
+++ database/migrations/2023_09_16_200425_create_alunos_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('alunos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome')->nullable();
+            $table->string('CPF', 11)->unique()->nullable();
+            $table->char('sexo', 2)->nullable();
+            $table->string('email', 150)->unique()->nullable();
+            $table->float('rendaMensal', 8, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('alunos');
+    }
+};
